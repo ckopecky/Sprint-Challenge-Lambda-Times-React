95d9951f439f0dc30f9b79516f3bfbaa6cff964e
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:22:38 -0700

    CardContainer styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+      width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+    <CardContainer>
           {props.cards.map(item => {
             return (
               <Card key={item.headline + item.author} card={item} />
             )
           })}
-    </div>
+    </CardContainer>
   )
 }
 
