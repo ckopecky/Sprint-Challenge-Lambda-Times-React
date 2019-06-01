76e24bade13017593c1c660898b6b5e456857126
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:22:12 -0700

    SingleCard component

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const SingleCard = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+  > div:nth-child(1) {
+    font-size: 25px;
+    font-family: Didot, serif;
+  }
+  > div:nth-child(2) {
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 15px;
+    > div {
+      padding-right: 10px;
+      border-right: 1px solid lightgrey;
+      height: 40px;
+      img {
+        width: 40px;
+      }  
+    }
+    span {
+      padding-left: 10px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+    }
+  }
+`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <SingleCard>
+      <div>{props.card.headline}</div>
+      <div>
+        <div>
           <img src={props.card.img} alt={props.card.author}/>
         </div>
         <span>By {props.card.author}</span>
       </div>
-    </div>
+    </SingleCard>
   );
 };
 
