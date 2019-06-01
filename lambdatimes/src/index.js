281d1e2d1b64a5dbc8ed7c5a83da5ad5455f22de
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:21:00 -0700

    WrapperComponent styled component

--- lambdatimes/src/index.js
+++ lambdatimes/src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './CSS/index.css';
+// import './CSS/index.css';
+import styled from 'styled-components';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WrapperComponent = styled.body`
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: #f1f1f1;
+    color: #333;
+    font-family: 'PT Sans', sans-serif;
+    * {
+        box-sizing: border-box;
+    }
+`
+
+ReactDOM.render(
+<WrapperComponent>
+    <App />
+</WrapperComponent>, document.getElementById('root'));
