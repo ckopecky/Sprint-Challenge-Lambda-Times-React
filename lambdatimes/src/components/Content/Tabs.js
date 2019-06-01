24b2db7743e6481e1b2946745542641e5d210d45
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:23:31 -0700

    StyledTabs container styled component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledTabs = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+  >div {
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    > span:first-child {
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+    }
+  }
+`
 
 const Tabs = props => {
   return (
-    <div className="tabs">
+    <StyledTabs>
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
@@ -19,7 +45,7 @@ const Tabs = props => {
                      />
             })}
       </div>
-    </div>
+    </StyledTabs>
   );
 };
 
