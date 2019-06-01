f7c9848156a5f84dba240f536839714a7a115e4a
ckopecky <kopecky12112@gmail.com>
2019-05-31 22:45:52 -0700

    add props to arg list and invoked date func

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <span className="date">{props.date()}</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
