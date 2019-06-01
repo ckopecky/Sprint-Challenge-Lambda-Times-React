e304265b63cbc5c09c73b6fc91dc4b634a5f2e4a
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:20:44 -0700

    App Wrapper Styled Component

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
+
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`;
 
 const App = () => {
   let date = new Date();
@@ -10,11 +20,11 @@ const App = () => {
     return current;
   }
   return (
-    <div className="App">
+    <AppWrapper>
       <TopBar />
       <Header date={getDateString}/>
       <Content />
-    </div>
+    </AppWrapper>
   );
 }
 
