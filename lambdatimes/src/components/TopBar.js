dcc9d066c4cca81b9a9175aa108d46ea9f2cce66
ckopecky <kopecky12112@gmail.com>
2019-06-01 16:21:53 -0700

    TopBar Styled Component

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,92 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarComponent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  > div {
+    width: 100%;
+    display: flex;
+    justify-content: none;
+    align-items: none;
+    flex-direction: row;
+    color: #fff;
+    letter-spacing: 1px;
+    padding: 0 10px;
+    @media (min-width: 1280px) {
+      width: 1280px;
+    }
+    > div:nth-child(1) {
+      display: flex;
+      justify-content: none;
+      align-items: center;
+      flex-direction: row;
+      flex: 1;
+      font-size: 11px;
+      span {
+        cursor: pointer;
+        margin-right: 25%;
+        font-weight: bold;
+      }
+    }
+    >div:nth-child(2) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row;
+      flex: 3;
+      font-size: 9px;
+      span {
+        cursor: pointer;
+        margin-right: 5%;
+      }
+      span:last-child {
+        margin-right: 0;
+      }
+      span:hover {
+        text-decoration: underline;
+      }
+    }
+    > div:nth-child(3) {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      flex-direction: row;
+      flex: 1;
+      font-size: 11px;
+      font-weight: bold;
+      span {
+        cursor: pointer;
+      }
+    }
+
+  }
+`
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopBarComponent>
+      <div>
+        <div>
           <span>TOPICS</span><span>SEARCH</span>
         </div>
-        <div className="container-center">
+        <div>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
         </div>
-        <div className="container-right">
+        <div>
           <span>LOG IN</span>
         </div>
       </div>
-    </div>
+    </TopBarComponent>
   )
 }
 
