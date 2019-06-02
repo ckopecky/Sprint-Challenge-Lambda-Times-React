6c544b6c1f105a8f166f940d8c101e5786bab568
ckopecky <kopecky12112@gmail.com>
2019-06-01 20:30:21 -0700

    add styled components

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,6 +1,62 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+const StyledCarousel = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  > div {
+      display: flex;
+      justify-content: center;
+      align-items: none;
+      flex-direction: row;
+      color: #fff;
+      background-color: #333;
+      font-size: 40px;
+      border-radius: 50%;
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      cursor: pointer;
+      &:hover {
+        color: #333;
+        background-color: #fff;
+        border: 2px solid #333;
+      }
+      &:first-child {
+        top: 50%;
+        left: 25px;
+        transform: translate(0, -50%);
+      }
+      &:last-child {
+        top: 50%;
+        right: 25px;
+        transform: translate(0, -50%);
+      }
+  }
+  img {
+    width: 100%;
+    display: none;
+  }
+
+`
+
+
+
+
+
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
